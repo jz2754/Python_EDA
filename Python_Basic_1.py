c0051ab661ba9d2ed7a759027937e36d2e1515c9
jz2754 <36461045+jz2754@users.noreply.github.com>
2023-08-05 13:37:31 -0400

    python practice

---
+++ Python_Basic_1.py
@@ -0,0 +1,21 @@
+
+# print statement
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("****")
+print("***")
+print("**")
+print("*")
+
+
+
+# variables
+
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
